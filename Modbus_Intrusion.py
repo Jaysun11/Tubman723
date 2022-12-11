58fd7e40c28c5b3173e552aff071b70b9d420f83
Jason Tubman <jason.tubman2@gmail.com>
2022-12-11 20:20:45 +1000

    Trying to read packet and parse - can't yet find modbus packets

--- Modbus_Intrusion.py
+++ Modbus_Intrusion.py
@@ -1,14 +1,25 @@
 from scapy.all import *
 import struct
+import datetime
+
+#if set, the server_address will be used, if not the entire network will be sniffed
+MANUAL = False
+
+#dictionary used to store Modbus Packets
+modbus_packets_processed = {}
 
 # Define the Socket Details (if MANUAL boolean set)
 server_address = '192.168.1.114'
 
-MANUAL = False
+# Get current time in iso format
+def get_iso_time():
+    local_timezone = datetime.datetime.now().replace(microsecond=0).isoformat()
+    return local_timezone
+
 
 def print_packet_details(slave_id, function_code, start_address, num_registers):
     #print packet details
-    print("Modbus Packet Details")
+    print("Processing Packet Details")
     print(slave_id)
     print(function_code)
     print(start_address)
@@ -16,13 +27,33 @@ def print_packet_details(slave_id, function_code, start_address, num_registers):
 
 def parse_modbus_packet(payload):
 
-    slave_id = struct.unpack('>B', payload[6:7])[0]
+    print('12 Byte payload recieved, verifying format...')
+
+    # Parse Modbus TCP information
+    transaction_id = struct.unpack('>H', payload[0:2])[0]
+    protocol_id = struct.unpack('>H', payload[2:4])[0]
+    length = struct.unpack('>H', payload[4:6])[0]
+    unit_id = struct.unpack('>B', payload[6:7])[0]
+
+    # Parse Modbus Data ADU
     function_code = struct.unpack('>B', payload[7:8])[0]
     start_address = struct.unpack('>H', payload[8:10])[0]
     num_registers = struct.unpack('>H', payload[10:12])[0]
 
-    print_packet_details(slave_id, function_code, start_address, num_registers)
-    check_modbus_validity(slave_id, function_code, start_address, num_registers)
+    #store packet in a dictionary with current time
+    packet_data =  {
+        'timestamp': get_iso_time(),
+        'transaction_id': transaction_id,
+        'protocol_id': protocol_id,
+        'length': length,
+        'unit_id': unit_id,
+        'function_code': function_code,
+        'start_address': start_address,
+        'number_of_registers': num_registers
+    }
+
+    print_packet_details(packet_data)
+    check_modbus_validity(packet_data)
 
 def check_modbus_validity(slave_id, function_code, start_address, num_registers):     
     
@@ -46,13 +77,15 @@ def check_modbus(packet):
         if payload is not None:
             try:
 
-                print('Payload Recieved, has TCP header, checking if it was a Modbus request')
-        
                 # Check if data is a Modbus request by looking at attributes of payload
-                if len(payload) >= 12 and struct.unpack('>B', payload[7:8])[0] == 3:
-                    print("here")
+                if len(payload) == 12:
+
+                    """
+                        If the payload is 12 bytes long it is quite likely a modbus packet
+                        Parse the packet to verify
+                    """
                     parse_modbus_packet(payload)                
-                    
+
             except:
                 #Packet was not a TCP Packet, cant be modbus so PASS
                 pass
@@ -61,6 +94,6 @@ def check_modbus(packet):
 
 # Start sniffing the network traffic
 if (not MANUAL):
-    sniff(prn=check_modbus)
+    sniff(filter="tcp", prn=check_modbus)
 else:
-    sniff(filter="ip and host " + server_address, prn=check_modbus)
+    sniff(filter="ip and host " + server_address + " tcp", prn=check_modbus)
