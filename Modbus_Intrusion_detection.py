2a6d8a99323a55bea50ead5a81858941e14e4bca
Jason Tubman <jason.tubman2@gmail.com>
2023-01-10 13:21:07 +0800

    Add files via upload

--- Modbus_Intrusion_detection.py
+++ Modbus_Intrusion_detection.py
@@ -3,6 +3,7 @@ import struct
 from datetime import datetime
 import time
 
+
 #if set, the server_address will be used, if not the entire network will be sniffed
 MANUAL = False
 
@@ -34,8 +35,8 @@ TIME_FACTOR = 2
 
 # Set the threshold for the number of connections within a certain time period
 #This is used to identify a DOS attack
-CONNECTION_THRESHOLD = 100
-TIME_PERIOD = 60  # seconds
+CONNECTION_THRESHOLD = 150
+TIME_PERIOD = 20 # seconds
 
 # Define the Socket Details (if MANUAL boolean set)
 server_address = '127.0.0.1'
@@ -53,7 +54,7 @@ def print_packet_details(packet_data):
 
 def parse_modbus_packet(payload, source_IP_ADDRESS):
 
-    print('12 Byte payload recieved, verifying format...')
+    #print('12 Byte payload recieved, verifying format...')
     payload = bytes(payload)
     # Parse Modbus TCP information
 
@@ -82,7 +83,7 @@ def parse_modbus_packet(payload, source_IP_ADDRESS):
     }
 
     check_modbus_validity(packet_data)
-    print_packet_details(packet_data)
+    
 
 def check_modbus_validity(packet_data):     
     
@@ -93,6 +94,8 @@ def check_modbus_validity(packet_data):
         print("Function Code : " + str(packet_data['unit_id']))
         print("Start Address: " + str(packet_data['unit_id']))
         print("Number Of Registers : " + str(packet_data['number_of_registers']))
+        
+        print_packet_details(packet_data)
 
         
         with open("modbus_false_packet_detected.txt", "a") as f:  # open the file in append mode
@@ -115,7 +118,7 @@ def check_modbus_validity(packet_data):
         #calculate time disparity, if too short, could be a MITM attack
         record_packet(packet_data)
         sys.stdout.flush()
-        check_time_disparity()
+        check_time_disparity(packet_data)
         sys.stdout.flush()
         check_if_first_time_origin(packet_data)
         sys.stdout.flush()
@@ -128,7 +131,11 @@ def check_if_first_time_origin(packet_data):
     global ip_counter
 
     if(ip_counter[packet_data['origin_ip']] == 1):
+        
         print('Possible Modbus intrusion detected! (First time recieved from origin)')
+        print_packet_details(packet_data)
+        sys.stdout.flush()
+        
         with open("modbus_mitm_detected.txt", "a") as f:  # open the file in append mode
             f.write("MITM (FIRST TIME) ATTACK DETECTED: \n")  # write the current timestamp to the file then a new line
             # Get the current date and time
@@ -151,7 +158,7 @@ def remove_up_to_character(string, character):
     # Return the string with all characters up to the first occurrence of the character removed
     return string[index + 1:]
 
-def check_time_disparity():
+def check_time_disparity(packet_data):
     global modbus_packets_processed
 
     if (len(modbus_packets_processed) >=3):
@@ -165,11 +172,14 @@ def check_time_disparity():
             Tcurr = (T3 - T2).total_seconds()
             Tdisp = abs(Tprev - Tcurr)
 
-            print("TDiSP: ", Tdisp)
+            #
         
             if (Tdisp > TIME_FACTOR):
                 last_3_packets[2]['alert'] = True
+                print("TDiSP: ", Tdisp)
+                print_packet_details(packet_data)
                 print('Possible Modbus intrusion detected (MITM)!')
+                sys.stdout.flush()
                 with open("modbus_mitm_detected.txt", "a") as f:  # open the file in append mode
                     f.write("MITM ATTACK DETECTED: \n")  # write the current timestamp to the file then a new line
                     # Get the current date and time
@@ -208,17 +218,20 @@ def check_modbus(packet):
     #check IP
     source_IP_ADDRESS = packet[IP].src
 
-    #reset values after time threshold    
-    if (start_time - time.time() > TIME_PERIOD):
-        start_time = time.time()
-        ip_counter = {}
+    #reset values after time threshold  
+
+    if (time.time() - start_time > TIME_PERIOD):
         dos_warning = False
+        start_time = time.time()
+        ip_counter[source_IP_ADDRESS] = 1
+        
 
     else:
         try:
             if (ip_counter[source_IP_ADDRESS] >= CONNECTION_THRESHOLD and dos_warning == False):
                 #If there has CONNECTION_THRESHOLD to many connections in the last TIME_THRESHOLD seconds.
                 print("Warning! Possible DOS attack")
+                
                 dos_warning = True
                 with open("modbus_dos_detected.txt", "a") as f:  # open the file in append mode
                     f.write("DOS ATTACK DETECTED: \n")  # write the current timestamp to the file then a new line
@@ -230,6 +243,7 @@ def check_modbus(packet):
 
                     f.write(str(date_time) + "\n")  # write the current timestamp to the file then a new line
         except KeyError:
+            #print("KEY ERROR on DOS Attack")
             pass
 
     
